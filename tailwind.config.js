45c04eb8f8f6ca32c46a57c082cae0803f2a4fb5
Rishabh Pathak <124903626+rishabhpathak359@users.noreply.github.com>
2023-12-24 22:16:24 +0530

    Revert "Mentee LeaderBoard"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,11 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#004AAD',
-        secondary: '#38b6ff',
-        customGray: 'rgba(78, 82, 88, 0.4)',
-      },
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-        custom: ['Poppins', 'sans-serif'],
-      },
-      container: {
-        center: true,
-        padding: '1rem',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
