ddfddf6e5154e7f044d868675948d27e7d6c0d32
Rishabh <rishabh09p@gmail.com>
2023-12-24 22:55:49 +0530

    ..

--- src/data/MenteeData.jsx
+++ src/data/MenteeData.jsx
@@ -1,63 +1,82 @@
 export const mentees = [
     {
-      "id": 1,
-      "name": "Mentee 1",
-      "problemsSolved": 20,
-      "pointsScored": 180
+        "id": "🥇",
+        "name": "Mentee 1",
+        "problemsSolved": 20,
+        "pointsScored": 180,
+        "achievements": ["Top Performer", "Problem Solver"],
+        "teamName": "Team A"
     },
     {
-      "id": 2,
-      "name": "Mentee 2",
-      "problemsSolved": 15,
-      "pointsScored": 150
+        "id": "🥈",
+        "name": "Mentee 2",
+        "problemsSolved": 15,
+        "pointsScored": 150,
+        "achievements": ["Most Improved"],
+        "teamName": "Team B"
     },
     {
-      "id": 3,
-      "name": "Mentee 3",
-      "problemsSolved": 25,
-      "pointsScored": 250
+        "id": "🥉",
+        "name": "Mentee 3",
+        "problemsSolved": 25,
+        "pointsScored": 250,
+        "achievements": ["Leaderboard Champion"],
+        "teamName": "Team A"
     },
     {
-      "id": 4,
-      "name": "Mentee 4",
-      "problemsSolved": 18,
-      "pointsScored": 170
+        "id": 4,
+        "name": "Mentee 4",
+        "problemsSolved": 18,
+        "pointsScored": 170,
+        "achievements": ["Code Master"],
+        "teamName": "Team C"
     },
     {
-      "id": 5,
-      "name": "Mentee 5",
-      "problemsSolved": 30,
-      "pointsScored": 270
+        "id": 5,
+        "name": "Mentee 5",
+        "problemsSolved": 30,
+        "pointsScored": 270,
+        "achievements": ["Bug Hunter"],
+        "teamName": "Team B"
     },
     {
-      "id": 6,
-      "name": "Mentee 6",
-      "problemsSolved": 22,
-      "pointsScored": 200
+        "id": 6,
+        "name": "Mentee 6",
+        "problemsSolved": 22,
+        "pointsScored": 200,
+        "achievements": ["Innovator"],
+        "teamName": "Team A"
     },
     {
-      "id": 7,
-      "name": "Mentee 7",
-      "problemsSolved": 27,
-      "pointsScored": 230
+        "id": 7,
+        "name": "Mentee 7",
+        "problemsSolved": 27,
+        "pointsScored": 230,
+        "achievements": ["Quality Assurance Expert"],
+        "teamName": "Team C"
     },
     {
-      "id": 8,
-      "name": "Mentee 8",
-      "problemsSolved": 35,
-      "pointsScored": 300
+        "id": 8,
+        "name": "Mentee 8",
+        "problemsSolved": 35,
+        "pointsScored": 300,
+        "achievements": ["Puzzle Solver"],
+        "teamName": "Team A"
     },
     {
-      "id": 9,
-      "name": "Mentee 9",
-      "problemsSolved": 28,
-      "pointsScored": 260
+        "id": 9,
+        "name": "Mentee 9",
+        "problemsSolved": 28,
+        "pointsScored": 260,
+        "achievements": ["Top Analyst"],
+        "teamName": "Team B"
     },
     {
-      "id": 10,
-      "name": "Mentee 10",
-      "problemsSolved": 33,
-      "pointsScored": 290
+        "id": 10,
+        "name": "Mentee 10",
+        "problemsSolved": 33,
+        "pointsScored": 290,
+        "achievements": ["Creative Coder"],
+        "teamName": "Team C"
     }
-  ];
-  
+];
