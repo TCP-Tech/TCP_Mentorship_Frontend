1a128108378ad16f01c77782688dc08dec764bf2
AviralPatel0526 <aviralpatel2603@gmail.com>
2023-12-25 10:01:02 +0530

    Navbar added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import LeaderBoard from '../components/LeaderBoard/TeamLeaderBoard'
 import MenteeLeaderBoard from '../components/LeaderBoard/MenteeLeaderBoard'
-
+import Navbar from '../components/Navbar'
 const Home = () => {
   return (
     <div>
+        <Navbar/>
         <LeaderBoard/>
         <MenteeLeaderBoard/>
     </div>
