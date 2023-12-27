c4816ccd611cdb1480f2089e61819ddc4455f342
Rishabh Pathak <124903626+rishabhpathak359@users.noreply.github.com>
2023-12-27 16:42:00 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,21 +13,4 @@ const Home = () => {
         <MenteeLeaderBoard />
       </div>
       <Footer />
-
-import React from 'react'
-import LeaderBoard from '../components/LeaderBoard/TeamLeaderBoard'
-import MenteeLeaderBoard from '../components/LeaderBoard/MenteeLeaderBoard'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-const Home = () => {
-  return (
-    <div>
-        <Navbar/>
-        <LeaderBoard/>
-        <MenteeLeaderBoard/>
-        <Footer/>
-    </div>
-  );
-};
-
 export default Home;
