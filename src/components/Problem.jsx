e5e4e0f1f49bd7b1261ff8fa19378a0a36d9f7b8
Praveen Patro <128630671+praveen-p09@users.noreply.github.com>
2023-12-30 19:01:29 +0530

    Added mentor dashboard

---
+++ src/components/Problem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Problem = ({ title,desc,topic, level, url }) => {
+  return (
+    <div className="flex flex-col sm:flex-row items-center  dark:bg-gray-800 dark:border-white justify-between border border-gray-400 rounded-lg m-2">
+      <a href={url} target="_blank"><div className="flex flex-col p-2">
+        <h1 className="m-4 text-2xl dark:text-white  font-semibold">{title}</h1>
+        <h3 className="m-4 text-md dark:text-white">{desc}</h3>
+      </div></a>
+      <div className="flex flex-col items-center p-4">
+        <button className="bg-[var(--primary-c)] text-white rounded-lg dark:border dark:border-white p-3 m-3">Mark as Done</button>
+        <div className="flex">
+            <p className="border dark:bg-gray-800 dark:text-white dark:border-white border-[var(--primary-c)] rounded-lg text-sm p-1 m-1">{topic}</p>
+            <p className="border dark:bg-gray-800 dark:text-white dark:border-white border-[var(--primary-c)] rounded-lg text-sm p-1 m-1">{level}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Problem;
---
+++ src/components/ProblemList.jsx
@@ -0,0 +1,18 @@
+import Problem from "./Problem";
+import AddProblem from "./AddProblem";
+import React,{ useState, useEffect } from "react";
+
+const ProblemList = () => {
+  
+  return (
+    <div className="flex flex-col  space-x-8">
+       <h1 className="text-3xl text-black pt-7 pb-5 font-semibold">Problems Assigned</h1>
+      <div className="flex flex-col">
+        <Problem title="Subset Sum" topic="Recursion" level="Easy" desc="Lorem ipsum" url="https://leetcode.com/problems/partition-equal-subset-sum/"/>
+        <Problem title="Subset Sum" topic="Recursion" level="Easy" desc="Lorem ipsum" url="https://leetcode.com/problems/partition-equal-subset-sum/"/>
+        <Problem title="Subset Sum" topic="Recursion" level="Easy" desc="Lorem ipsum" url="https://leetcode.com/problems/partition-equal-subset-sum/"/>
+      </div>
+    </div>
+  );
+};
+export default ProblemList;
