a0f5d5a45f9f425e6501e5b8ca6d83fb8e09897c
Rishabh <rishabh09p@gmail.com>
2023-12-31 18:58:22 +0530

    LeaderBoard Changes

--- src/components/TeamCompo.jsx
+++ src/components/TeamCompo.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const TeamCompo = ({ teamName, teamLeaderName, teamPoints, id, teamDp }) => {
   return (
-    <div className="flex items-center justify-between  py-3 ">
+    <div className="flex items-center justify-between py-3 ">
       <div className="flex items-center">
-        <h1 className="mr-4">{id}</h1>
-        <div className="rounded-full overflow-hidden bg-gray-300 w-12 h-12 mr-2"></div>
+        <h1 className="mr-4 dark:text-gray-400 text-gray-600 ">{id}</h1>
+        <div className="rounded-full overflow-hidden dark:bg-gray-800 w-12 h-12 mr-2"></div>
         <div className="cursor-pointer">
-          <h1 className="font-semibold">{teamName}</h1>
-          <h2 className="text-gray-500 text-sm">{teamLeaderName}</h2>
+          <h1 className="md:font-semibold text-black dark:text-white">{teamName}</h1>
+          <h2 className="dark:text-gray-400 text-gray-600 text-sm">{teamLeaderName}</h2>
         </div>
       </div>
-      <h1 className="font-semibold">{teamPoints} Points</h1>
+      <h1 className="md:font-semibold text-black dark:text-white text-sm md:text-lg">{teamPoints} Points</h1>
     </div>
   );
 };
