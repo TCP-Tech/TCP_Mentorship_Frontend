57c2a2ce891cdb5b00f0535387636dd828e96d5e
Rishabh <rishabh09p@gmail.com>
2024-01-13 16:21:45 +0530

    Api integrated,Private Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import React from "react";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
@@ -7,6 +9,7 @@ import Home from "./pages/Home";
 import MenteeDashBoard from "./pages/DashBoard/MenteeDashBoard";
 import MentorDashboard from "./pages/DashBoard/MentorDashboard";
 import NotFoundPage from "./pages/NotFoundPage";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   const [isDarkMode, setDarkMode] = useState(false);
@@ -27,13 +30,15 @@ const App = () => {
   }, [isDarkMode]);
   return (
     <Router>
+        <ToastContainer 
+        position="bottom-right" />
       <Routes>
         <Route exact path="*" element={<NotFoundPage />} />
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<PrivateRoute element={<Home />} />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/mentee" element={<MenteeDashBoard />} />
-        <Route exact path="/mentor" element={<MentorDashboard />} />
+        <Route exact path="/mentee" element={<PrivateRoute element={<MenteeDashBoard />} />} />
+        <Route exact path="/mentor" element={<PrivateRoute element={<MentorDashboard />} />} />
       </Routes>
     </Router>
   );
