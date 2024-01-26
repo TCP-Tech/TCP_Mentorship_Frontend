b1c3e17bc9d2a768f718316d1e8d50663a74838e
Ayush shukla <ayushshukla3999@gmail.com>
2024-01-27 01:36:28 +0530

    team leaderboard implemented

--- src/components/MenteesCard.jsx
+++ src/components/MenteesCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const MenteesCompo = ({ name, problemsSolved, points, id, DP }) => {
+const MenteesCompo = ({ name,image, problemsSolved, points, id, DP }) => {
   return (
     <div className="flex items-center justify-between   py-3">
       <div className="flex items-center">
         <h1 className="mr-4">{id}</h1>
-        <div className="rounded-full overflow-hidden bg-gray-300 w-12 h-12 mr-2"></div>
+        <div className="rounded-full overflow-hidden bg-gray-300 w-12 h-12 mr-2">
+          <img src={image} alt="" />
+        </div>
         <div className="ml-4">
           <h1 className="font-semibold text-black">{name}</h1>
           <h1 className="text-gray-500">Problems Solved: {problemsSolved}</h1>
