99a779f3786063c3ab57c4e787a429ddb145f8de
Rishabh <rishabh09p@gmail.com>
2024-02-12 04:10:15 +0530

    Create Team and dash changes

---
+++ src/components/DashBoardComponents/MenteeCodingProfiles.jsx
@@ -0,0 +1,194 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Modal from 'react-modal';
+import MenteesUrlsCard from '../MenteesUrlsCard';
+import close from '../../assets/images/cross.svg';
+import closelight from '../../assets/images/cross.png';
+import { toast } from 'react-toastify';
+import { fetchDataFromApi, fetchDataFromApiWithResponse } from '../../utils/api';
+
+const MenteeCodingProfiles = ({ mentor,mentee }) => {
+  const [menteeData, setMenteeData] = useState();
+  const [showModal, setShowModal] = useState(false);
+  const [teamCreated,setTeamCreated] = useState(false);
+  const [teamData, setTeamData] = useState();
+  const [teamData1, setTeamData1] = useState();
+  const inputRef = useRef();
+  const fetchData = async () => {
+    try {
+      const data = await fetchDataFromApi( "get-team-mentor",mentor? mentor?.id : mentee?.mentor_id);
+
+      setTeamData(data.team_data);
+      setTeamData1(data);
+      
+    } catch (error) {
+      console.error("Error fetching team data:", error.message);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [mentee]);
+
+  const handleClick = (member) => {
+    setMenteeData(member);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleTeamCreation =async () => {
+    const body={
+        teamname:inputRef.current.value,
+        mentorid:mentor?.id
+    }
+    const data = await fetchDataFromApiWithResponse( body,'createteam' );
+    if(data.status_code==200){
+      toast.success("Team Created Successfully", {
+        position: "bottom-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      fetchData();
+    }
+    else{
+      toast.error("Some Error Occured ! Please try again.", {
+        position: "bottom-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl pt-7 md:pb-4 pb-8 md:text-left text-center text-black dark:text-white font-semibold ">
+       { mentor ? "Mentee Profiles" : "Team Members"}
+      </h1>
+      
+      {teamData && teamData.length>0 ? (
+        <>
+          <div className="dark:bg-gray-800  overflow-y-auto h-[34.5vh] rounded-lg dark:border-none border">
+          {teamData1 && (
+                <h2 className={`md:text-2xl text-lg font-semibold text-center mb-2 bg-primary text-white p-2 py-3 `}>
+                 {teamData[0].team_name}
+                </h2>
+              )}
+            {teamData?.map((team) => (
+              <div key={team.id}>
+                {team.team_members.map((member) => (
+                  <div
+                    key={member.id}
+                    className="flex items-center justify-between py-3 px-3 border-b cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700"
+                    onClick={() => handleClick(member)}
+                  >
+                    <div className="flex items-center justify-between w-full">
+                      <div className="rounded-full overflow-hidden dark:bg-gray-800 w-10 h-10 mr-2">
+                        <img
+                          className="h-10 w-10 object-cover rounded-full"
+                          src={member.image}
+                          alt={`${member.name}-dp`}
+                        />
+                         
+                      </div>
+                      <h1 className="text-sm md:font-semibold text-black dark:text-white">
+                          {member.name}
+                        </h1>
+                      <div>
+                       
+                        <h2 className="text-xs dark:text-gray-400 text-gray-600">
+                          Score: {member.score}
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+
+          <Modal
+            isOpen={showModal}
+            onRequestClose={closeModal}
+            className="dark:bg-gray-800 mx-auto mt-8 sm:mt-2 bg-gray-100 p-4"
+            style={{
+              overlay: {
+                zIndex: 10000,
+              },
+              content: {
+                width: '90%',
+                maxWidth: '700px',
+                height: '80%',
+              },
+            }}
+          >
+            <button onClick={closeModal} className="dark:bg-gray-800 bg-gray-100">
+              <img
+                alt="close"
+                src={close}
+                className="w-5 h-5 hidden dark:block"
+              />
+              <img alt="close" src={closelight} className="w-4 h-4 dark:hidden" />
+            </button>
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="font-bold text-center text-black md:text-2xl text-xl dark:text-white">
+                {menteeData ? menteeData?.name : 'Mentee'}'s Profile
+              </h1>
+              <div className="team-profile-members overflow-y-scroll w-full h-[80vh] mt-12">
+                {menteeData && (
+                  <MenteesUrlsCard
+                    image={menteeData.image}
+                    name={menteeData.name}
+                    codechefID={menteeData.codechefID}
+                    codeforcesID={menteeData.codeforcesID}
+                    leetcodeID={menteeData.leetcodeID}
+                    gfgID={menteeData.gfgID}
+                    hackerrankID={menteeData.hackerrankID}
+                    points={menteeData.score}
+                    solvedQ={menteeData.solvedQ}
+                  />
+                )}
+              </div>
+            </div>
+          </Modal>
+        </>
+      ) : mentor ? (
+        <div className="flex flex-col items-center justify-center h-[35vh] border dark:border:none">
+          <h1 className="dark:text-white text-black mb-4">
+            No teams have been created yet
+          </h1>
+          <input
+            type="text"
+            ref={inputRef}
+            placeholder="Enter the team name to be created"
+            className="border border-gray-300 p-2 rounded-md mb-2 bg-white text-black"
+          />
+          <button
+            onClick={handleTeamCreation}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+          >
+            Create
+          </button>
+        </div>
+      ) : (
+        <div className="flex justify-center items-center text-center border h-[35vh] px-8">
+          <h1 className="text-sm dark:text-white text-black">
+            Looks like your mentor hasn't created a team yet! Please wait for them to create one.
+          </h1>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MenteeCodingProfiles;
--- src/components/DashBoardComponents/MentorDefaultDash.jsx
+++ src/components/DashBoardComponents/MentorDefaultDash.jsx
@@ -1,86 +1,51 @@
 import React,{useEffect, useState} from "react";
-import Modal from "react-modal";
-import MenteesUrlsCard from "../MenteesUrlsCard";
-import Profile from "./Profile";
-import close from "../../assets//images/cross.svg";
-import closelight from "../../assets//images/cross.png";
-import { fetchDataFromApi } from "../../utils/api";
+import MenteeCodingProfiles from "./MenteeCodingProfiles";
 const MentorDefaultDash = () => {
   const [mentor] = useState(JSON.parse(localStorage.getItem("Mentor")));
   const Level=(mentor.Qlevel_count);
   const Topic=(mentor.topic_count);
-  const [teamData, setTeamData] = useState();
-  const [teamData1, setTeamData1] = useState();
-  const [menteeData, setMenteeData] = useState();
-  const [showModal, setShowModal] = useState(false);
-  const fetchData = async () => {
-    try {
-      const data = await fetchDataFromApi( "get-team-mentor",mentor.id);
-
-      setTeamData(data.team_data);
-      setTeamData1(data);
-      
-    } catch (error) {
-      console.error("Error fetching team data:", error.message);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const handleClick = (teamData) => {
-    setMenteeData(teamData);
-    console.log(teamData);
-    setShowModal(true);
-  };
-  const closeModal = () => {
-    setShowModal(false);
-  };
-
   return (
     <>
-      <div>
-        <h1 className="text-3xl text-center md:text-left text-black dark:text-white md:pt-5 pb-4 md:pb-1 md:px-2 font-semibold">
-          Overview
+      <div className="flex flex-col md:flex-row md:-mt-4">
+  {/* Overview */}
+  <div className="flex flex-col md:w-1/2 p-4">
+    <h1 className="text-3xl text-center md:text-left text-black dark:text-white md:pt-5 pb-5 md:px-2  font-semibold">
+      Overview
+    </h1>
+    <div className="grid grid-cols-2 gap-4">
+      <div className="flex flex-col rounded-lg p-4 bg-red-100">
+        <h1 className="text-lg font-semibold mb-2 text-black">Questions Assigned</h1>
+        <h1 className="border-b-4 w-1/2 border-red-500 text-2xl py-2 font-bold text-black">
+          {mentor?.total_q}
         </h1>
-        <div className="flex flex-col md:flex-row space-y-3 justify-center items-center">
-           <div className="grid grid-cols-2 md:w-[50%]">
-           <div className="flex flex-col rounded-lg p-4 m-2 bg-red-100 md:mr-2">
-           <h1 className="md:text-lg font-semibold mb-2 text-black">Questions Assigned</h1>
-            <h1 className="border-b-4 w-1/2 border-red-500 text-2xl py-2 font-bold text-black">
-              {mentor.total_q}
-              </h1>
-            </div>
-            <div className="flex flex-col rounded-lg p-4 m-2 bg-green-100 md:mr-2">
-              <h1 className="md:text-lg font-semibold mb-2 text-black">
-                Team Score
-              </h1>
-              <h1 className="border-b-4 w-1/2 border-green-500 text-2xl py-2 font-bold text-black">
-              {mentor.score}
-              </h1>
-            </div>
-            <div className="flex flex-col rounded-lg p-4 m-2 bg-blue-100 md:mr-2">
-              <h1 className="md:text-lg font-semibold mb-2 text-black">
-                Team Ranking
-              </h1>
-              <h1 className="border-b-4 w-1/2 border-blue-500 text-2xl py-2 font-bold text-black">
-                120
-              </h1>
-            </div>
-            <div className="flex flex-col rounded-lg p-4 m-2 bg-violet-100 md:mr-2">
-              <h1 className="md:text-lg font-semibold mb-2 text-black">Streak</h1>
-              <h1 className="border-b-4 w-1/2 border-violet-500 text-2xl py-2 font-bold text-black">
-                0
-              </h1>
-            </div>
-          </div>
-          <div className="w-[100%] md:w-[50%]">
-            <Profile mode="mentor" teamData={teamData1}/>
-          </div>
-        </div>
       </div>
+      <div className="flex flex-col rounded-lg p-4 bg-green-100">
+        <h1 className="text-lg font-semibold mb-2 text-black">Team Score</h1>
+        <h1 className="border-b-4 w-1/2 border-green-500 text-2xl py-2 font-bold text-black">
+          { mentor?.score }
+        </h1>
+      </div>
+      <div className="flex flex-col rounded-lg p-4 bg-blue-100">
+        <h1 className="text-lg font-semibold mb-2 text-black">Team Ranking</h1>
+        <h1 className="border-b-4 w-1/2 border-blue-500 text-2xl py-2 font-bold text-black">
+          120
+        </h1>
+      </div>
+      <div className="flex flex-col rounded-lg p-4 bg-violet-100">
+        <h1 className="text-lg font-semibold mb-2 text-black">Streak</h1>
+        <h1 className="border-b-4 w-1/2 border-violet-500 text-2xl py-2 font-bold text-black">
+          0
+        </h1>
+      </div>
+    </div>
+  </div>
+  <div className="w-full md:w-1/2 p-4">
+          <MenteeCodingProfiles mentor={mentor}/>
+    </div>
+ </div>
+
       <div>
-        <h1 className="text-3xl pt-7 pb-5  text-black dark:text-white font-semibold">
+      <h1 className="text-3xl text-center md:text-left text-black dark:text-white md:pt-5 pt-10 pb-5 md:px-2  font-semibold">
           Problem Stats
         </h1>
         <div className="flex flex-col">
@@ -105,7 +70,7 @@ const MentorDefaultDash = () => {
             </div>
           </div>
         </div>
-        <h1 className="text-3xl text-black  dark:text-white pt-7 pb-5 font-semibold">
+        <h1 className="text-3xl text-center md:text-left text-black dark:text-white md:pt-5 pt-12 pb-5 md:px-2  font-semibold">
           Topics covered
         </h1>
         <div className="flex flex-col">
@@ -214,93 +179,7 @@ const MentorDefaultDash = () => {
             </div>
           </div>
         </div>
-
       </div>
-      <div>
-  <h1 className="text-3xl pt-7 pb-5 text-black dark:text-white font-semibold">
-    Mentee Coding Stats
-  </h1>
-  <div className="dark:bg-gray-800 overflow-y-scroll px-10 mt-4 h-[70vh] w-full rounded-tl-[40px] rounded-tr-[40px] border dark:border-gray-600">
-    {teamData?.map((team) => (
-      <div key={team.id}>
-        {team.team_members.map((member) => (
-          <div key={member.id} onClick={() => handleClick(member)}>
-            <div className="flex items-center justify-between py-3 mt-6 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700">
-              <div className="flex items-center space-x-2">
-                <div className="rounded-full overflow-hidden dark:bg-gray-800 w-12 h-12 mr-2">
-                <img className="h-12 w-12 object-cover rounded-full" src={member.image} alt={`${member.name}-dp`} />
-                </div>
-                <div>
-                  <h1 className="md:font-semibold text-black dark:text-white">
-                    {member.name}
-                  </h1>
-                  <h2 className="dark:text-gray-400 text-gray-600 text-sm">
-                    {member.team_name}
-                  </h2>
-                </div>
-              </div>
-              <h1 className="md:font-semibold text-black dark:text-white text-sm md:text-lg">
-                {member.score} Points
-              </h1>
-            </div>
-            <div className="h-0 w-full border border-zinc-300 dark:border-zinc-600 border-opacity-50"></div>
-          </div>
-        ))}
-      </div>
-    ))}
-  </div>
-</div>
-
-          <div className="flex items-center justify-center">
-          <Modal
-        isOpen={showModal}
-        onRequestClose={closeModal}
-        className="dark:bg-gray-800 mx-auto mt-[8%] sm:mt-[2%] bg-gray-100 p-4"
-        style={{
-          overlay: {
-            zIndex: 10000,
-          },
-          content: {
-            width:"90%",
-            maxWidth: "700px",
-            height:"80%",
-          },
-        }}
-      >
-        <button onClick={closeModal} className="dark:bg-gray-800 bg-gray-100">
-          <img
-            alt="close"
-            src={close}
-            className="w-5 h-5 hidden dark:block"
-          />
-          <img
-            alt="close"
-            src={closelight}
-            className="w-4 h-4 dark:hidden"
-          />
-        </button>
-        <div className="flex flex-col items-center justify-center">
-        <h1 className="font-bold text-center text-black md:text-2xl text-xl dark:text-white">
-          {menteeData? menteeData?.name : "Mentee"}'s Profile
-        </h1>
-        <div className="team-profile-members overflow-y-scroll h-[80vh] mt-12">
-          {menteeData && (
-            <MenteesUrlsCard
-              image={menteeData.image}
-              name={menteeData.name}
-              codechefID={menteeData.codechefID}
-              codeforcesID={menteeData.codeforcesID}
-              leetcodeID={menteeData.leetcodeID}
-              gfgID={menteeData.gfgID}
-              hackerrankID={menteeData.hackerrankID}
-              points={menteeData.score}
-              solvedQ={menteeData.solvedQ}
-            />
-          )}
-        </div>
-        </div>
-      </Modal>
-          </div>
     </>
   );
 };
