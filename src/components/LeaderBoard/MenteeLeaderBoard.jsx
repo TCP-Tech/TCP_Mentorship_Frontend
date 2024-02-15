8d47a488783e5827078aa58400608c517172b9c5
Praveen Patro <128630671+praveen-p09@users.noreply.github.com>
2024-02-15 23:48:21 +0530

    fixes

--- src/components/LeaderBoard/MenteeLeaderBoard.jsx
+++ src/components/LeaderBoard/MenteeLeaderBoard.jsx
@@ -31,11 +31,16 @@ const MenteeLeaderBoard = () => {
    }, [])
 
    MenteeData?.sort(function (mentee1, mentee2) {
-    if(  mentee2?.score == mentee1?.score){
-     return mentee2?.cumHour_diff - mentee1?.cumHour_diff;
+    if(  mentee2?.score === mentee1?.score){
+      if(mentee2?.solvedQ === mentee1?.solvedQ){
+        return mentee1?.cumHour_diff - mentee2?.cumHour_diff
+      }
+      else{
+        return mentee2?.solvedQ - mentee1?.solvedQ;
+      }
     }
     else{
-     return mentee2?.score - mentee1?.score;
+      return mentee2?.score - mentee1?.score;
     }
     });
    
